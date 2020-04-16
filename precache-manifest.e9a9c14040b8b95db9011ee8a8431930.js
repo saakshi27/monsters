245f88b1d4987afe1a1c29bb886a751f5d5008cd
saakshi27 <saakshidikshit@gmail.com>
2020-04-16 11:44:38 +0530

    Updates

---
+++ precache-manifest.e9a9c14040b8b95db9011ee8a8431930.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6db98c682b91a7658b1e0280989f86",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c9d37499897c9b2bca6e",
+    "url": "/monsters/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "de7ee7d6de8b7d8c4ad7",
+    "url": "/monsters/static/css/main.85cc1811.chunk.css"
+  },
+  {
+    "revision": "c9d37499897c9b2bca6e",
+    "url": "/monsters/static/js/2.c74e940c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.c74e940c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de7ee7d6de8b7d8c4ad7",
+    "url": "/monsters/static/js/main.1206f0dd.chunk.js"
+  },
+  {
+    "revision": "01f8084b1bc817b79899",
+    "url": "/monsters/static/js/runtime-main.4bfacb6d.js"
+  }
+]);
